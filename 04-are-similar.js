ff11e83968fe65d9358b95b132b44baadc813a7c
Razafimamonjisoa Mendrika Harizo <98638405+Mendrika27@users.noreply.github.com>
2022-03-23 14:11:09 +0300

    feat: calculation of temporal complexity of T04

--- 04-are-similar.js
+++ 04-are-similar.js
@@ -4,10 +4,14 @@
  * Given two arrays a and b, check whether they are similar.
  */
 const areSimilar = (a, b) => {
-  for (var arr = [], i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      arr.push(a[i], b[i]);
+  for (var arr = [], i = 0; i < a.length; i++) { //5a.length
+    if (a[i] !== b[i]) { //5
+      arr.push(a[i], b[i]);//5
     }
   }
-  return arr.length < 5 && new Set(arr).size < 3;
+  return arr.length < 5 && new Set(arr).size < 3; //4
 };
+/*
+*T_04 (n) = 5*a(5+5)+4 = 5*a(10)+4 = 1*n(1)+1 = n
+*T_04 (n) = n = O (n) (linear)
+*/
