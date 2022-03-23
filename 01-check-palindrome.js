dd7ad66403357f43a548220151338332d6520d49
Razafimamonjisoa Mendrika Harizo <98638405+Mendrika27@users.noreply.github.com>
2022-03-23 13:38:57 +0300

    feat: calculation of temporal complexity of T01

--- 01-check-palindrome.js
+++ 01-check-palindrome.js
@@ -1,2 +1,6 @@
 const checkPalindrome = (inputString) =>
-  inputString === inputString.split("").reverse().join("");
+  inputString === inputString.split("").reverse().join(""); // 4
+/**
+*T_01 (n) = 4 (constante)
+*T_01 (n) = 1 = 0 (1)
+*/
