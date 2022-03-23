a6ab20aad4946ded7b99698f050b567738f4f158
Razafimamonjisoa Mendrika Harizo <98638405+Mendrika27@users.noreply.github.com>
2022-03-23 15:50:34 +0300

    feat: calculation of temporal complexity of T06

--- 06-deposit-profit.js
+++ 06-deposit-profit.js
@@ -5,4 +5,10 @@
  * find out how long it would take for your balance to pass a specific threshold.
  */
 const depositProfit = (deposit, rate, threshold) =>
-  Math.ceil(Math.log(threshold / deposit) / Math.log(rate / 100 + 1));
+  Math.ceil(Math.log(threshold / deposit) / Math.log(rate / 100 + 1)); //(deposit=d; rate=r; threshold=t) 1 ((t*1+1*d)+1+1(r+3)) 
+/*
+*T_06 (n) = 1((t*1+1*d)+1+1(r+3))
+*T_06 (n) = t+d+1+r+1
+*T_06 (n) = n+n+1+n+1 = 3n = n
+*T_06 (n) = n = O (n) (linear)
+*/
